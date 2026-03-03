cb588d1897751f6b6a9ef11cc98ac953661d70d6
Buğra Çalışkandinç <bugracaliskandinc@gmail.com>
2026-03-03 09:15:48 +0300

    Added Github Pages & Actions configs/files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bugracdnc.github.io',
+  base: '/neslihanogretmenim', // This must match your GitHub repository name exactly
+
   vite: {
     plugins: [tailwindcss()]
   },
