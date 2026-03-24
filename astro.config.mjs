6d7ec1df91571d26f875a17fe4907eb8000d6cba
Buğra Çalışkandinç <bugracaliskandinc@gmail.com>
2026-03-24 22:54:03 +0300

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://neslihanogretmenim.pages.dev',
+  site: 'https://neslihanogretmenim.com',
   base: '/',
   trailingSlash: 'never',
 
