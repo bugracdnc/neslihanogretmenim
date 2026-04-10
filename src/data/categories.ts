9bf75b0dac8b51dce4087d18c31a10685f4cc92d
Buğra Çalışkandinç <bugracaliskandinc@gmail.com>
2026-04-10 20:57:41 +0300

    Updated card images and image urls

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -8,31 +8,31 @@ export const categories: Category[] = [
     {
         name: "1. Sınıf",
         slug: "1-sinif",
-        image: "/images/categories/sinif-1.jpg",
+        image: "/images/categories/sinif-1.png",
     },
     {
         name: "2. Sınıf",
         slug: "2-sinif",
-        image: "/images/categories/sinif-2.jpg",
+        image: "/images/categories/sinif-2.png",
     },
     {
         name: "3. Sınıf",
         slug: "3-sinif",
-        image: "/images/categories/sinif-3.jpg",
+        image: "/images/categories/sinif-3.png",
     },
     {
         name: "4. Sınıf",
         slug: "4-sinif",
-        image: "/images/categories/sinif-4.jpg",
+        image: "/images/categories/sinif-4.png",
     },
     {
         name: "Belirli Gün ve Haftalar",
         slug: "belirli-gunler",
-        image: "/images/categories/belirli.jpg",
+        image: "/images/categories/belirli.png",
     },
     {
         name: "Görsel Sanatlar",
         slug: "gorsel-sanatlar",
-        image: "/images/categories/sanat.jpg",
+        image: "/images/categories/sanat.png",
     },
 ];
